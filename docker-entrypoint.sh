fc293d27ad6271e09dcec8852071ad163ac046fb
Andrey Maslov <an_maslov@itsm.tech>
2026-02-24 22:17:50 +0200

    Add Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "Downloading proxy files..."
-curl -s https://core.telegram.org/getProxySecret -o /app/proxy-secret || exit 1
-curl -s https://core.telegram.org/getProxyConfig -o /app/proxy-multi.conf || exit 1
-
 echo "Starting mtproto-proxy..."
 exec mtproto-proxy --aes-pwd /app/proxy-secret /app/proxy-multi.conf "$@"
